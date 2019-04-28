a27261f7fdc1bb3adb049573841fd4c5124700ba
Lois Pangestu <loispangestu.id@gmail.com>
2019-04-28 17:43:08 +0700

    NEW

--- app/src/main/java/com/example/lois/leehype/LoginActivity.java
+++ app/src/main/java/com/example/lois/leehype/LoginActivity.java
@@ -9,10 +9,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        System.out.println("COMPLETE");
-        System.out.println("COMPLETE 2");
-        System.out.println("COMPLETE 3");
-        System.out.println("1. Branch");
     }
 }
